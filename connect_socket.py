5ef97d8f075f4563504c80064e154e27159d4d8b
Nguyễn Đức Hoàng <hoangnd.ute@gmail.com>
2024-09-06 01:22:12 +0700

    table view

--- connect_socket.py
+++ connect_socket.py
@@ -1,12 +1,11 @@
 import socket
 import json
-import os
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-server_address = ('3.18.29.6', 12345)
+server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 
 
@@ -14,33 +13,18 @@ def get_clients(client_socket):
     command = {
         'cmd': 'ls_clients'
     }
-    
     client_socket.sendall(json.dumps(command).encode())
     response = client_socket.recv(1024)
     response = json.loads(response.decode())
-    
     return response['result']
 
-
-print(get_clients(client_socket))
 
 
 def send_command(client_socket, command):
 
 # Send a message to the server
     try:
-        # lấy các clients đang kết nối
-        command = {
-            'cmd': 'ls_clients'
-        }
-        
-        client_socket.sendall(json.dumps(command).encode())
-        response = client_socket.recv(1024)
-        
-        print(response.decode())
-        
-        
-        
+
         """ command = {
             'cmd': 'python',
             'clients': ['118.70.190.129'],
@@ -56,3 +40,6 @@ def send_command(client_socket, command):
         client_socket.close()
     
     
+    
+if __name__ == '__main__':
+    print(get_clients(client_socket))
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from mainui_v2 import Ui_MainWindow
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QLineEdit
-from PyQt6.QtCore import QRect, QPropertyAnimation, QEasingCurve, QAbstractAnimation, QTimer
-from PyQt6.QtWidgets import QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QLineEdit, QTableWidgetItem, QCheckBox, QWidget, QHBoxLayout
+from PyQt6.QtCore import QRect, QPropertyAnimation, QEasingCurve, QAbstractAnimation, QTimer, Qt
+from PyQt6.QtGui import QPixmap, QIcon
+from PyQt6 import QtCore
 import sys
 import requests
 import json
@@ -9,6 +10,19 @@ import time
 import os
 import datetime
 import random
+from connect_socket import *
+
+def get_flag_url(country_code):
+    response = requests.get(f'https://restcountries.com/v3.1/alpha/{country_code}')
+    data = response.json()
+    flag_url = data[0]['flags']['png']
+    return flag_url
+
+def download_image(url):
+    response = requests.get(url)
+    image = QPixmap()
+    image.loadFromData(response.content)
+    return image
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -25,7 +39,10 @@ class MainWindow(QMainWindow):
         self.ui.stackedWidget.setFrameStyle(0)
     
         
-    
+        self.load_clients()
+        
+        
+        
         self.setup_log_generation()
         
     def setup_marquee(self):
@@ -48,14 +65,84 @@ class MainWindow(QMainWindow):
         # thiết lập kích thước cột cho tableWidget reponsive với kích thuớc cửa sổ tính theo %
         table_width = self.ui.tableWidget.width()
         self.ui.tableWidget.setColumnWidth(0, int(table_width * 0.01))
-        self.ui.tableWidget.setColumnWidth(1, int(table_width * 0.2))
-        self.ui.tableWidget.setColumnWidth(2, int(table_width * 0.4))
-        self.ui.tableWidget.setColumnWidth(3, int(table_width * 0.2))
+        self.ui.tableWidget.setColumnWidth(1, int(table_width * 0.3))
+        self.ui.tableWidget.setColumnWidth(2, int(table_width * 0.2))
+        self.ui.tableWidget.setColumnWidth(3, int(table_width * 0.3))
         self.ui.tableWidget.setColumnWidth(4, int(table_width * 0.3))
-        self.ui.tableWidget.setColumnWidth(5, int(table_width * 0.3))
+        self.ui.tableWidget.setColumnWidth(5, int(table_width * 0.2))
         self.ui.tableWidget.setColumnWidth(7, int(table_width * 0.2))
         
+    def load_clients(self):
+        clients = get_clients(client_socket)
+        print(clients)
         
+        self.ui.tableWidget.setRowCount(len(clients))
+        for i, client in enumerate(clients):
+            # cột 0 là ô checkbox
+            self.ui.tableWidget.setItem(i, 0, QTableWidgetItem())
+            self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(client['hostname']))
+            ip = QTableWidgetItem(client['ip'])
+            ip.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.ui.tableWidget.setItem(i, 2, ip)
+            country_code = client['country']
+            flag_url = get_flag_url(country_code)
+            flag_image = download_image(flag_url)
+            # Create QIcon from QPixmap and set it to QTableWidgetItem
+            flag_icon = QIcon(flag_image)
+            flag_item = QTableWidgetItem(f"{client['city']} - {client['country']}")
+            #flag_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            flag_item.setIcon(flag_icon)
+            self.ui.tableWidget.setItem(i, 3, flag_item)
+            
+            # tạo status có icon online trong text
+            status = QTableWidgetItem("🟢 Online")
+            status.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.ui.tableWidget.setItem(i, 5, status)
+            
+            view_screen = QTableWidgetItem("🖥️")
+            view_screen.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.ui.tableWidget.setItem(i, 6, view_screen)
+            
+            setting = QTableWidgetItem("⚙️")
+            setting.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.ui.tableWidget.setItem(i, 7, setting)
+            
+            
+            
+        
+            
+        self.load_select()
+    
+    
+    
+    
+    def load_select(self):
+        for i in range(self.ui.tableWidget.rowCount()):
+            checkbox = QCheckBox()
+            checkbox.setChecked(False)  # Initially unchecked
+            checkbox_layout = QWidget()
+            checkbox_layout.setLayout(QHBoxLayout())
+            checkbox_layout.layout().addWidget(checkbox)
+            checkbox_layout.layout().setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+            checkbox_layout.layout().setContentsMargins(0, 0, 0, 0)
+            self.ui.tableWidget.setCellWidget(i, 0, checkbox_layout)
+            
+    def push_select_all(self, tableWidget):
+        # kiểm tra xem tất cả row đã được chọn chưa
+        is_all_checked = True
+        for i in range(tableWidget.rowCount()):
+            checkbox = tableWidget.cellWidget(i, 0).layout().itemAt(0).widget()
+            if not checkbox.isChecked():
+                is_all_checked = False
+        # nếu tất cả row đã được chọn thì bỏ chọn tất cả
+        if is_all_checked:
+            for i in range(tableWidget.rowCount()):
+                checkbox = tableWidget.cellWidget(i, 0).layout().itemAt(0).widget()
+                checkbox.setChecked(False)
+        else:
+            for i in range(tableWidget.rowCount()):
+                checkbox = tableWidget.cellWidget(i, 0).layout().itemAt(0).widget()
+                checkbox.setChecked(True)
     
     
     
@@ -119,6 +206,8 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_7.clicked.connect(self.switch_to_monitor)
         self.ui.pushButton_4.clicked.connect(self.switch_to_setting)
         self.ui.pushButton_8.clicked.connect(self.switch_to_setting)
+        
+        self.ui.pushButton_11.clicked.connect(lambda: self.push_select_all(self.ui.tableWidget))
         
         
         
