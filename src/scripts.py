874cc667953c1071af3992fde0d1d38ed6249c7b
Nguyễn Đức Hoàng <hoangnd.ute@gmail.com>
2024-09-12 03:25:52 +0700

    mupdate

--- src/scripts.py
+++ src/scripts.py
@@ -2,6 +2,7 @@
 import keyboard
 from pynput import mouse
 from PyQt6.QtWidgets import QMessageBox, QLineEdit
+import subprocess
 
 # tạo 1 hàm lắng nghe phím bấm sau đó trả về ký tự tương ứng
 def listen_key():
@@ -24,6 +25,8 @@ class Scripts:
         code_python = '''# --------------------- ADD CHROME ---------------------
 import webbrowser
 webbrowser.open_new_tab('{}')
+import time
+time.sleep(1)
 # --------------------------------------------------------'''.format(link)
         # ghi thêm textedit ở đây
         self.ui.textEdit_3.append(code_python)
@@ -37,6 +40,40 @@ time.sleep({time} - random.randint(-{time_random}, {time_random}))
 # --------------------------------------------------------'''.format(time=time, time_random=time_random)
         # ghi thêm textedit ở đây
         self.ui.textEdit_3.append(code_python)
+    def add_chrome_new_undetected_chromedriver(self):
+        code_python = '''# --------------------- ADD CHROME NEW UNDETECTED CHROMEDRIVER ---------------------
+from undetected_chromedriver import Chrome, ChromeOptions
+options = ChromeOptions()
+driver = Chrome(options=options)
+import time
+time.sleep(1)
+# --------------------------------------------------------'''
+        self.ui.textEdit_3.append(code_python)
+        
+    def add_resize_chrome(self):
+        width = self.ui.lineEdit_95.text()
+        height = self.ui.lineEdit_96.text()
+        code_python = '''# --------------------- ADD RESIZE CHROME ---------------------
+driver.set_window_size({}, {})
+import time
+time.sleep(1)
+# --------------------------------------------------------'''.format(width, height)
+        self.ui.textEdit_3.append(code_python)
+    def add_maximize_chrome(self):
+        code_python = '''# --------------------- ADD MAXIMIZE CHROME ---------------------
+driver.maximize_window()
+import time
+time.sleep(1)
+# --------------------------------------------------------'''
+        self.ui.textEdit_3.append(code_python)
+    def add_zoom_chrome(self):
+        zoom = self.ui.lineEdit_98.text()
+        code_python = '''# --------------------- ADD ZOOM CHROME ---------------------
+driver.execute_script("document.body.style.zoom='{}%'")
+import time
+time.sleep(1)
+# --------------------------------------------------------'''.format(zoom)
+        self.ui.textEdit_3.append(code_python)
         
         
     # Tab Keyboard
@@ -48,16 +85,20 @@ time.sleep({time} - random.randint(-{time_random}, {time_random}))
         code_python = '''# --------------------- ADD KEY ---------------------
 import pyautogui
 pyautogui.press('{}')
+import time
+time.sleep(1)
 # --------------------------------------------------------'''.format(key)
         self.ui.textEdit_3.append(code_python)
     def add_text_keyboard(self):
         text = self.ui.lineEdit_72.text()
-        delay = self.ui.lineEdit_82.text()
-        code_python = '''# --------------------- ADD TEXT KEYBOARD ---------------------
+        code_python = f'''# --------------------- ADD TEXT KEYBOARD ---------------------
 import pyautogui
 import time
-pyautogui.typewrite('{}', interval={})
-# --------------------------------------------------------'''.format(text, delay)
+import random
+for s in '{text}':
+    pyautogui.typewrite(s, interval=random.randint(1,10)/50)
+time.sleep(1)
+# --------------------------------------------------------'''
         self.ui.textEdit_3.append(code_python)
     def get_key_multi(self):
         key = listen_key()
@@ -73,6 +114,8 @@ pyautogui.typewrite('{}', interval={})
         code_python = '''# --------------------- ADD KEY MULTI ---------------------
 import pyautogui
 pyautogui.hotkey('{}')
+import time
+time.sleep(1)
 # --------------------------------------------------------'''.format(keys_formatted)
         self.ui.textEdit_3.append(code_python)
         
@@ -102,9 +145,26 @@ pyautogui.hotkey('{}')
         code_python = '''# --------------------- ADD MOUSE MOVE RANDOM IN BOUNDARY ---------------------
 import pyautogui
 import random
-x = random.randint({}, {})
-y = random.randint({}, {})
-pyautogui.moveTo(x, y)
+import time
+import math
+def move_like_human(x_start, y_start, x_end, y_end, duration):
+    steps = random.randint(1, 7)  # Giảm số bước để di chuyển nhanh hơn
+    sleep_time = duration / steps
+    for step in range(steps):
+        t = step / (steps - 1)  # Đảm bảo t = 1 ở bước cuối cùng
+        ease_t = (math.sin((t - 0.5) * math.pi) + 1) / 2  # Hiệu ứng di chuyển mượt
+        x = x_start + (x_end - x_start) * ease_t
+        y = y_start + (y_end - y_start) * ease_t
+        
+        pyautogui.moveTo(x, y)
+        time.sleep(sleep_time)  # Thời gian nghỉ giữa mỗi bước
+    pyautogui.moveTo(x_end, y_end)
+
+x_start, y_start = pyautogui.position()
+x_end = random.randint({}, {})
+y_end = random.randint({}, {})
+move_like_human(x_start, y_start, x_end, y_end, duration=0.02)
+time.sleep(1)
 # --------------------------------------------------------'''.format(int(x), int(x) + int(width), int(y), int(y) + int(height))
         self.ui.textEdit_3.append(code_python)
         
@@ -114,19 +174,42 @@ pyautogui.moveTo(x, y)
         y = self.ui.lineEdit_84.text()
         code_python = '''# --------------------- ADD MOUSE MOVE ---------------------
 import pyautogui
-pyautogui.moveTo({}, {})
+import random
+import time
+import math
+def move_like_human(x_start, y_start, x_end, y_end, duration):
+    steps = random.randint(1, 7)  # Giảm số bước để di chuyển nhanh hơn
+    sleep_time = duration / steps
+    for step in range(steps):
+        t = step / (steps - 1)  # Đảm bảo t = 1 ở bước cuối cùng
+        ease_t = (math.sin((t - 0.5) * math.pi) + 1) / 2  # Hiệu ứng di chuyển mượt
+        x = x_start + (x_end - x_start) * ease_t
+        y = y_start + (y_end - y_start) * ease_t
+        
+        pyautogui.moveTo(x, y)
+        time.sleep(sleep_time)  # Thời gian nghỉ giữa mỗi bước
+    pyautogui.moveTo(x_end, y_end)
+x_start, y_start = pyautogui.position()
+x_end = {}
+y_end = {}
+move_like_human(x_start, y_start, x_end, y_end, duration=0.02)
+time.sleep(1)
 # --------------------------------------------------------'''.format(x, y)
         self.ui.textEdit_3.append(code_python)
     def add_mouse_right_click(self):
         code_python = '''# --------------------- ADD MOUSE RIGHT CLICK ---------------------
 import pyautogui
 pyautogui.rightClick()
+import time
+time.sleep(1)
 # --------------------------------------------------------'''
         self.ui.textEdit_3.append(code_python)
     def add_mouse_left_click(self):
         code_python = '''# --------------------- ADD MOUSE LEFT CLICK ---------------------
 import pyautogui
 pyautogui.leftClick()
+import time
+time.sleep(1)
 # --------------------------------------------------------'''
         self.ui.textEdit_3.append(code_python)
     def add_mouse_scroll(self):
@@ -135,6 +218,8 @@ pyautogui.leftClick()
         code_python = '''# --------------------- ADD MOUSE SCROLL ---------------------
 import pyautogui
 pyautogui.scroll({wheel})
+import time
+time.sleep(1)
 # --------------------------------------------------------'''.format(wheel=wheel)
         self.ui.textEdit_3.append(code_python)
         
@@ -144,10 +229,13 @@ pyautogui.scroll({wheel})
     def run_test(self):
         # lấy code từ textedit sau đó chạy
         code = self.ui.textEdit_3.toPlainText()
+        # lưu code vào file
+        with open('temp.py', 'w', encoding='utf-8') as file:
+            file.write(code)
         # tạo 1 thread chạy exec code để không block UI
         import threading
         def run_code():
-            exec(code)
+            subprocess.run(['python', 'temp.py'])
         threading.Thread(target=run_code).start()
     def delete_step(self):
         # xóa bước cuối cùng
@@ -202,6 +290,10 @@ pyautogui.scroll({wheel})
         self.ui.pushButton_61.clicked.connect(self.add_mouse_right_click)
         self.ui.pushButton_82.clicked.connect(self.add_mouse_scroll)
         self.ui.pushButton_86.clicked.connect(self.add_mouse_move_random_in_boundary)
+        self.ui.pushButton_26.clicked.connect(self.add_chrome_new_undetected_chromedriver)
+        self.ui.pushButton_95.clicked.connect(self.add_resize_chrome)
+        self.ui.pushButton_94.clicked.connect(self.add_maximize_chrome)
+        self.ui.pushButton_97.clicked.connect(self.add_zoom_chrome)
         
         self.ui.pushButton_22.clicked.connect(self.run_test)
         self.ui.pushButton_23.clicked.connect(self.delete_step)
---
+++ temp.py
@@ -0,0 +1,23 @@
+# --------------------- ADD MOUSE MOVE ---------------------
+import pyautogui
+import random
+import time
+import math
+def move_like_human(x_start, y_start, x_end, y_end, duration):
+    steps = random.randint(1, 7)  # Giảm số bước để di chuyển nhanh hơn
+    sleep_time = duration / steps
+    for step in range(steps):
+        t = step / (steps - 1)  # Đảm bảo t = 1 ở bước cuối cùng
+        ease_t = (math.sin((t - 0.5) * math.pi) + 1) / 2  # Hiệu ứng di chuyển mượt
+        x = x_start + (x_end - x_start) * ease_t
+        y = y_start + (y_end - y_start) * ease_t
+        
+        pyautogui.moveTo(x, y)
+        time.sleep(sleep_time)  # Thời gian nghỉ giữa mỗi bước
+    pyautogui.moveTo(x_end, y_end)
+x_start, y_start = pyautogui.position()
+x_end = 195
+y_end = 146
+move_like_human(x_start, y_start, x_end, y_end, duration=0.02)
+time.sleep(1)
+# --------------------------------------------------------
